e93ba75b17ef9a45c5346711cbfef1dd6609d324
Kevin <935485864@qq.com>
2015-05-08 01:20:22 +0800

    add ToolBar

--- app/src/main/java/com/kevinhan/meituan/Activity/StaggeredGridActivity.java
+++ app/src/main/java/com/kevinhan/meituan/Activity/StaggeredGridActivity.java
@@ -5,8 +5,11 @@ package com.kevinhan.meituan.Activity;
  */
 import java.util.ArrayList;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -23,7 +26,7 @@ import com.kevinhan.meituan.Data.SampleData;
 import com.kevinhan.meituan.R;
 import com.yalantis.phoenix.PullToRefreshView;
 
-public class StaggeredGridActivity extends Activity implements AbsListView.OnScrollListener, AbsListView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+public class StaggeredGridActivity extends ActionBarActivity implements AbsListView.OnScrollListener, AbsListView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private static final String TAG = "StaggeredGridActivity";
     public static final String SAVED_DATA_KEY = "SAVED_DATA";
@@ -35,13 +38,15 @@ public class StaggeredGridActivity extends Activity implements AbsListView.OnScr
     private boolean mHasRequestedMore;
     private SampleAdapter mAdapter;
 
+    private Toolbar mToolbar;
+
     private ArrayList<String> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sgv);
-
+        setToolBar();
         phoenx();
 
         setTitle("SGV");
@@ -152,6 +157,9 @@ public class StaggeredGridActivity extends Activity implements AbsListView.OnScr
         return true;
     }
 
+    /**
+     * 下拉刷新
+     */
     public void phoenx(){
         mPullToRefreshView = (PullToRefreshView) findViewById(R.id.pull_to_refresh);
         mPullToRefreshView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
@@ -163,6 +171,39 @@ public class StaggeredGridActivity extends Activity implements AbsListView.OnScr
                         mPullToRefreshView.setRefreshing(false);
                     }
                 }, REFRESH_DELAY);
+            }
+        });
+    }
+
+    /**
+     * 设置ToolBar
+     */
+    public void setToolBar(){
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setLogo(R.mipmap.ic_launcher);
+        mToolbar.setTitle("Group-Buying");// 标题的文字需在setSupportActionBar之前，不然会无效
+        mToolbar.setSubtitle("Home");
+        setSupportActionBar(mToolbar);
+        /* 这些通过ActionBar来设置也是一样的，注意要在setSupportActionBar(toolbar);之后，不然就报错了 */
+        // getSupportActionBar().setTitle("标题");
+        // getSupportActionBar().setSubtitle("副标题");
+        // getSupportActionBar().setLogo(R.drawable.ic_launcher);
+
+        /* 菜单的监听可以在toolbar里设置，也可以像ActionBar那样，通过Activity的onOptionsItemSelected回调方法来处理 */
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_settings:
+                        Toast.makeText(StaggeredGridActivity.this, "action_settings", 0).show();
+                        break;
+                    case R.id.action_share:
+                        Toast.makeText(StaggeredGridActivity.this, "action_share", 0).show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
             }
         });
     }
